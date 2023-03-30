d36f0751213f272a88b29e33c45dff00bd9bb303
zzrs123 <2296704779@qq.com>
2023-03-30 22:03:21 +0800

    finish struct&&enum12

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -2,13 +2,17 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+// 三种结构体
+// --1: 普通结构体：类似于C语言，声明写类型不写值，定义= + 结构体{成员名：值}
+// --2: 元组结构体：tuple(0,1,1);
+// --3: 单元结构体
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    red:i32,
+    green:i32,
+    blue:i32,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(i32,i32,i32);
 
 #[derive(Debug)]
 struct UnitLikeStruct;
@@ -18,9 +22,13 @@ mod tests {
     use super::*;
 
     #[test]
+    
     fn classic_c_structs() {
-        // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct{
+            red:0,
+            green:255,
+            blue:0,
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -30,8 +38,8 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
-
+        let green =(0,255,0);
+        
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
         assert_eq!(green.2, 0);
@@ -40,7 +48,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
