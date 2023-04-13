c7ca189a293b82d997a91512af3513b411071209
zzrs123 <2296704779@qq.com>
2023-04-13 22:33:50 +0800

    learn tests

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,15 +14,43 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+// pub struct ReportCard {
+//     pub grade: f32,
+//     pub student_name: String,
+//     pub student_age: u8,
+// }
+
+/*
+ 需要修改ReportCard结构体，将grade字段改为泛型类型 T，
+ 然后在实现中修改print方法，使其能够处理f32和char类型的grade。
+ 然后在测试用例中使用一个字母类型的成绩 A+ 来测试。
+ */
+
+// pub struct  ReportCard{
+//     pub grade: T,
+//     pub student_name: String,
+//     pub student_age: u8,
+// }
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
+// impl ReportCard {
+//     pub fn print(&self) -> String {
+//         format!("{} ({}) - achieved a grade of {}",
+//             &self.student_name, &self.student_age, &self.grade)
+//     }
+// }
 
-impl ReportCard {
+impl ReportCard<f32> {
+    pub fn print(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade)
+    }
+}
+impl ReportCard<&str> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -50,7 +78,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
